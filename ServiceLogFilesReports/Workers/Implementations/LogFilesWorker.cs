08536bb13195150d9ffa37041e575eda4ea86e6d
george <badjokeresult@gmail.com>
2023-04-22 10:31:12 +0300

    anonymize-email-method-provided

--- ServiceLogFilesReports/Workers/Implementations/LogFilesWorker.cs
+++ ServiceLogFilesReports/Workers/Implementations/LogFilesWorker.cs
@@ -1,5 +1,5 @@
-using System.Net;
 using System.Text.RegularExpressions;
+
 using ServiceLogFilesReports.Workers.Abstractions;
 
 namespace ServiceLogFilesReports.Workers.Implementations;
@@ -8,15 +8,13 @@ public class LogFilesWorker : IFilesWorker
 {
     public IEnumerable<string> GetListOfFiles(Regex pattern, string path)
     {
-        var fullPath = GetAbsolutePathToFilesDir(path);
-
         var files = new List<string>();
 
-        foreach (var filePath in Directory.GetFiles(fullPath))
+        foreach (var filePath in Directory.GetFiles(path))
         {
             var file = filePath.Split('/').Last();
             if (pattern.IsMatch(file))
-                files.Add(file);
+                files.Add(path + "/" + file);
         }
 
         return files;
@@ -37,54 +35,8 @@ public class LogFilesWorker : IFilesWorker
         return filesByNames;
     }
 
-    public void AddLineToFile(string line, string file, int? maxFileSize = null, int? maxRotationsAmount = null)
-    {
-        if (!File.Exists(file))
-        {
-            WriteToNewFile(line, file, maxFileSize, maxRotationsAmount);
-            ProvideFilesRotation();
-        }
-
-        throw new NotImplementedException();
-    }
-
-    private void WriteToNewFile(string line, string file, int? maxFileSize = null, int? maxRotationsAmount = null)
+    public void AnonymizeEmailsInFiles(string file)
     {
         throw new NotImplementedException();
-    }
-
-    private void ProvideFilesRotation()
-    {
-        throw new NotImplementedException();
-    }
-
-    /*private string GetLineWithAnonimyzedEmailIfThere(string line)
-    {
-        var pattern = new Regex(
-            @"^((([0-9A-Za-z]{1}[-0-9A-z\.]{0,30}[0-9A-Za-z]?)|([0-9А-Яа-я]{1}[-0-9А-я\.]{0,30}[0-9А-Яа-я]?))@([-A-Za-z]{1,}\.){1,}[-A-Za-z]{2,})$");
-
-        var text = line.Split(' ').Last();
-        var words = text.Split(' ');
-        
-        var email = string.Empty;
-        foreach (var word in words)
-        {
-            if (pattern.IsMatch(word))
-            {
-                email = word;
-                break;
-            }
-        }
-        
-        
-    }*/
-
-    private string GetAbsolutePathToFilesDir(string path)
-    {
-        var absolutePathPattern = new Regex(@"(/)|(\w:)|(~)");
-
-        if (absolutePathPattern.IsMatch(path.Substring(0, 2)))
-            return path;
-        return Environment.CurrentDirectory + "/" + path;
     }
 }
